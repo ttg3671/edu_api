7460987072e23fc55a5c6d44cd43d16a01d2affc
ttg3671 <ttg3671@gmail.com>
2026-09-19 23:18:56 +0530

    fix logger

--- controllers/logout.controllers.js
+++ controllers/logout.controllers.js
@@ -40,7 +40,7 @@ export const signOut = asyncHandler(async (req, res) => {
   const user_id = req.user?.id;
   const cookies = req.cookies;
 
-  logger.info("cookies: ", req.cookies);
+  logger.info(`cookies: ${JSON.stringify(req.cookies)}`, { cookies: req.cookies });
 
   if (!cookies?.XXAFIT) {
     return res.sendStatus(204);
@@ -48,10 +48,10 @@ export const signOut = asyncHandler(async (req, res) => {
 
   const refreshToken = cookies.XXAFIT;
 
-  logger.info("logout device", { device_id: req.body.device_id });
+  logger.info(`logout device_id: ${req.body.device_id}`, { device_id: req.body.device_id });
 
   const deviceFp = generateDeviceFingerprint(req.body.device_id);
-  logger.info("logout device fingerprint", { deviceFp });
+  logger.info(`logout deviceFp: ${deviceFp}`, { deviceFp });
 
   const db = await dbConnectionPromise; 
 
